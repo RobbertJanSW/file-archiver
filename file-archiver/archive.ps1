dbd35985ff5b57467039e043a40c7a489bdde31d
RobbertJanSW <RobbertJanSW@users.noreply.github.com>
2019-12-13 10:03:18 +0100

    Support paths with spaces

--- file-archiver/archive.ps1
+++ file-archiver/archive.ps1
@@ -124,8 +124,10 @@ function archive($archiveObj, $defaults = $null) {
 	  log "Archiving file $($_.FullName) with LastWriteTime $($_.LastWriteTime) to archive $($archiveFullPath)"
 	  
 	  if (($filesArchiving.Count -gt 99) -Or (($currentArchiveCaching -ne $Null) -And ($currentArchiveCaching -ne $archiveFullPath))) {
-	   $filesString = "$($filesArchiving | % { $_.FullName })"
-	   iex "$global:sevenzipBinary a $archiveFullPath $($filesString)" | Out-Null
+	   $filesString = "$($filesArchiving | % { write-output `"'$($_.FullName)'`"})"
+       log $filesString
+	   log "& `"$global:sevenzipBinary`" a `"$archiveFullPath`" $($filesString)"
+	   iex "& `"$global:sevenzipBinary`" a `"$archiveFullPath`" $($filesString)" | Out-Null
         if ($error -ne $null) { throw $error; exit 77 }
         verifyContent $archiveFullPath $filesArchiving
 	    if ($error) { throw "Error occured - 3267" }
@@ -138,8 +140,10 @@ function archive($archiveObj, $defaults = $null) {
   }
 
 if ($filesArchiving.Count -ne 0) {
-	   $filesString = "$($filesArchiving | % { $_.FullName })"
-	   iex "$global:sevenzipBinary a $archiveFullPath $($filesString)" | Out-Null
+	   $filesString = "$($filesArchiving | % { write-output `"'$($_.FullName)'`"})"
+       log $filesString
+	   log "& `"$global:sevenzipBinary`" a `"$archiveFullPath`" $($filesString)"
+	   iex "& `"$global:sevenzipBinary`" a `"$archiveFullPath`" $($filesString)" | Out-Null
         if ($error -ne $null) { throw $error; exit 77 }
         verifyContent $archiveFullPath $filesArchiving
 	    if ($error) { throw "Error occured - 3267" }
